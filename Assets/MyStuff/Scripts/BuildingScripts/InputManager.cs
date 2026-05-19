583e8054fd550377e214f9348e51c934962c5845
Ngalisteo <nicolasgalisteo@gmail.com>
2026-05-19 15:16:43 +0200

    commented placementsystem and inputmanager(building one lol)

--- Assets/MyStuff/Scripts/BuildingScripts/InputManager.cs
+++ Assets/MyStuff/Scripts/BuildingScripts/InputManager.cs
@@ -5,52 +5,58 @@ using UnityEngine.EventSystems;
 public class InputManager : MonoBehaviour
 {
     [SerializeField]
-    private Camera sceneCamera;
+    private Camera sceneCamera; //basically we get the main camera in this case.
 
-    private Vector3 lastPosition;
+    private Vector3 lastPosition; //this will store the last valid position the raycast hit.
 
     [SerializeField]
-    private LayerMask placementLayerMask;
+    private LayerMask placementLayerMask; //this basically tells the raycast to only hit that layer and ignore the others (in this case Placement layer)
 
-    private PlayerInputActions action;
+    private PlayerInputActions action; // for input actions, new input system
 
 
-    public event Action OnClicked, OnExit;
+    public event Action OnClicked, OnExit; //These are events, it notifies that something happened, they take no parameters and they return nothing
 
-    private void Update()
+
+    private void Awake()
     {
-        if (action.BuildingSystem.PlaceItem.WasPressedThisFrame())
-            OnClicked?.Invoke();
+        action = new PlayerInputActions(); // just the enable for inputs, always put on awake
+    }
+
+
+    private void OnEnable() //start listening for input
+    {
+        action.Enable();
+    }
+
+    private void OnDisable() //stops listening for input
+    {
+        action.Disable();
+    }
+
+    private void Update()// this runs every frame
+    {
+        if (action.BuildingSystem.PlaceItem.WasPressedThisFrame()) //this returns true on exactly one frame, the moment the button is pressed. in this case, placeitem is left click.
+            OnClicked?.Invoke(); //the ?. means that only call invoke if someone is subscribed to this event, if not, do nothing
+
+        //same for this
         if (action.BuildingSystem.Escap.WasPressedThisFrame())
             OnExit?.Invoke();
     }
 
     public bool IsPointerOverUI()
-        => EventSystem.current.IsPointerOverGameObject();
+        => EventSystem.current.IsPointerOverGameObject(); //the arrow is just a short for return, this checks if the mouse is over a ui element, like the button to summon an iron mine
 
-    private void Awake()
-    {
-        action = new PlayerInputActions();
-    }
-
-    private void OnEnable()
-    {
-        action.Enable();
-    }
-
-    private void OnDisable()
-    {
-        action.Disable();
-    }
     public Vector3 GetSelectedMapPosition()
     {
-        Vector2 mousePos = action.BuildingSystem.CursorPosition.ReadValue<Vector2>();
-        Ray ray = sceneCamera.ScreenPointToRay(mousePos);
+        Vector2 mousePos = action.BuildingSystem.CursorPosition.ReadValue<Vector2>(); //reads the current mouse 2d screen position in pixels, like (676, 69)
+        Ray ray = sceneCamera.ScreenPointToRay(mousePos); //the camera shoots a raycast, like a lazer pointer.
+
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 100, placementLayerMask))
+        if (Physics.Raycast(ray, out hit, 100, placementLayerMask))//this makes the ray fire, up to 100 units away, out hit means that fills the variable with what you hit, in this case it will fill the variable with the exact 3d point position coordinate thats under the mouse
         {
-            lastPosition = hit.point;
+            lastPosition = hit.point; //lets store the last position the ray hit.
         }
-        return lastPosition;
+        return lastPosition; //returns the last hit position, if the mouse stops touching the ground, it keeps the last position instead of going insane
     }
 }
--- Assets/MyStuff/Scripts/BuildingScripts/PlacementSystem.cs
+++ Assets/MyStuff/Scripts/BuildingScripts/PlacementSystem.cs
@@ -7,75 +7,103 @@ public class PlacementSystem : MonoBehaviour
 {
 
     [SerializeField]
-    private InputManager inputManager;
+    private InputManager inputManager; //reference to inputmanager, need it to subscribe to onclicked and onexit
     [SerializeField]
     private Grid grid;
 
     [SerializeField]
-    private ObjectsDatabaseSO database;
+    private ObjectsDatabaseSO database; //my item database
 
     [SerializeField]
-    private GameObject gridVisualization;
+    private GameObject gridVisualization; //visual grid i see on the ground when entering buildmode
 
     //   private GridData floorData, furnitureData; //change this later
-    private GridData placedObjectsData;
-
-
+    private GridData placedObjectsData; //This tracks which grid cells are occupied, every time i place an item it registers it here
 
     [SerializeField]
-    private PreviewSystem preview;
+    private PreviewSystem preview; //ghostpreview that follows my cursors when im in build mode.
 
-    private Vector3Int lastDetectedPosition = Vector3Int.zero;
+    private Vector3Int lastDetectedPosition = Vector3Int.zero; //Store the last grid cell the mouse was on
 
     [SerializeField]
-    private ObjectPlacer objectPlacer;
+    private ObjectPlacer objectPlacer; //this that physically spawns and destroys the gameobjects(items) in the scene.
 
-    IBuildingState buildingState;
+    IBuildingState buildingState; //This is the state pattern interface, it holds placementstate and removingstate
 
 
     void Start()
     {
-        StopPlacement();
+        StopPlacement(); // makes sure the building mode is hidden and inactive
       //  floorData = new();
        // furnitureData = new();
-        placedObjectsData = new();
+        placedObjectsData = new(); //crates a fresh empty grid data trackers, to see which cells are occupied
     }
 
 
-    public void StartPlacement(int ID)
+    public void StartPlacement(int ID) //this is called by the ui buttons, it passes the id and starts placing the items with that ID
     {
-        StopPlacement();
-        gridVisualization.SetActive(true);
-        buildingState = new PlacementState(ID,
+        StopPlacement(); //cleans every previous state, so if i was already placing something it resets and prevents bugs.
+        gridVisualization.SetActive(true); //shows the grid on the ground.
+        buildingState = new PlacementState(ID, //creates a new placement state and passes everything it needs, so from now on it knows which item im placing and how to handle it
                                            grid,
                                            preview,
                                            database,
                                            placedObjectsData,
                                            objectPlacer);
-        inputManager.OnClicked += PlaceStructure;
-        inputManager.OnExit += StopPlacement;
+        inputManager.OnClicked += PlaceStructure; //basically this is where we subscribe to the events, its basically telling the input manager: when the player clicks, call this method.
+        inputManager.OnExit += StopPlacement; //same but when the player presses escape
+    } //Inputmanager doesnt know this exists, it just subscribes to the event thats in there, so it "hears" it and runs the method we assigned in for.
+
+
+    private void PlaceStructure() //this runs whenever the player licks, because placestructure is subscribed to the onclicked event.
+    {
+        if (inputManager.IsPointerOverUI()) //basically, if we're clicking a UI button, do nothing and return early, this stops from placing stuff "behind" the button
+        {
+            return;
+        }
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition(); //gets the 3D world position under the cursor, using the raycast from inputmanager.
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition); //converts that world position into a grid cell adress, like (2.5, 0, 1.5) is converted to (2, 0, 1)
+
+        buildingState.OnAction(gridPosition);// tells the current state to do whatever its supposed to, so if we're building it builds, if removing it removes lol
     }
 
-    public void StartRemoving()
+    private void StopPlacement()
+    {
+        if (buildingState == null) //if nothing is active just do nothing, prevents crashes
+            return;
+        gridVisualization.SetActive(false); //hides the grid
+        buildingState.EndState(); //tells the current state to cleans itself up, like destroying the preview ghost for example.
+        inputManager.OnClicked -= PlaceStructure; //this unsubscribes from the events, without this place structure would keep firing every click, even if i stopped placing.
+        inputManager.OnExit -= StopPlacement; //same, remember to always unsubscribe when im done.
+        lastDetectedPosition = Vector3Int.zero; //resets the last cell pos so next time i enter the first cell it detects its the correct one
+        buildingState = null; //clears the state so update knows nothing is active.
+
+    }
+
+    private void Update()
+    {
+        if (buildingState == null) //if buildingstate is null returns, it doesnt need to do anything.
+            return;
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition(); //gets mouse world position
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition); //converts it to cell
+        if (lastDetectedPosition != gridPosition) //only runs the logic when the mouse has moved to a different cell.
+        {
+            buildingState.UpdateState(gridPosition); //tells the active state to update the preview position and the color
+            lastDetectedPosition = gridPosition; //stores the current cell so next frame can be compared again.
+        }
+
+    }
+
+    public void StartRemoving() //same as startplacement but for removing, no ID needed since we're not selecting an item, just entering remove mode. creates a removingstate instead of placementstate.
     {
         StopPlacement();
         gridVisualization.SetActive(true );
         buildingState = new RemovingState(grid, preview, placedObjectsData, objectPlacer);
-        inputManager.OnClicked += PlaceStructure;
+        inputManager.OnClicked += PlaceStructure; //still subscribes to the same events, clicking will now remove instead of place because removingstate handles onaction differently
         inputManager.OnExit += StopPlacement;
     }
 
-    private void PlaceStructure()
-    {
-        if (inputManager.IsPointerOverUI())
-        {
-            return;
-        }
-        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
-        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
-
-      buildingState.OnAction(gridPosition);
-    }
+    
 
     //private bool CheckPlacementValidity(Vector3Int gridPosition, int selectedObjectIndex)
     //{
@@ -86,30 +114,6 @@ public class PlacementSystem : MonoBehaviour
     //    return placedObjectsData.CanPlaceObjectAt(gridPosition, database.objectsData[selectedObjectIndex].Size);
     //}
 
-    private void StopPlacement()
-    {
-        if(buildingState == null)
-            return;
-        gridVisualization.SetActive(false);
-        buildingState.EndState();
-        inputManager.OnClicked -= PlaceStructure;
-        inputManager.OnExit -= StopPlacement;
-        lastDetectedPosition = Vector3Int.zero;
-        buildingState = null;
+   
 
-    }
-
-    private void Update()
-    {
-        if (buildingState == null)
-            return;
-        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
-        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
-        if(lastDetectedPosition != gridPosition)
-        {
-            buildingState.UpdateState(gridPosition);
-            lastDetectedPosition = gridPosition;
-        }
-
-    }
 }
